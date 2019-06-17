29f20b9ef17ff13b32970a961a404e2db02204ce
humwawe <wawe8963098@163.com>
2019-06-17 19:28:40 +0800

    191

---
+++ src/main/java/number/of/one/bits/NumberOfOneBits.java
@@ -0,0 +1,20 @@
+package number.of.one.bits;
+
+import java.util.BitSet;
+
+/**
+ * @author hum
+ */
+public class NumberOfOneBits {
+    public int hammingWeight(int n) {
+        String str = Integer.toBinaryString(n);
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
