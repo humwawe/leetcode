af7a774500c90afc40e27e9842062c55943359fa
humwawe <wawe8963098@163.com>
2019-06-26 21:31:40 +0800

    219

---
+++ src/main/java/contains/duplicate/II/ContainsDuplicateII.java
@@ -0,0 +1,23 @@
+package contains.duplicate.II;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author hum
+ */
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
