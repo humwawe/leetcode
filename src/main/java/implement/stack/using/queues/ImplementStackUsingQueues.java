9f468548ee326058a5fa5990727e18a4212f5299
humwawe <wawe8963098@163.com>
2019-07-05 23:36:40 +0800

    225

---
+++ src/main/java/implement/stack/using/queues/ImplementStackUsingQueues.java
@@ -0,0 +1,42 @@
+package implement.stack.using.queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author hum
+ */
+public class ImplementStackUsingQueues {
+
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public ImplementStackUsingQueues() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+
+    }
+
+
+    public int pop() {
+        return queue1.poll();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
