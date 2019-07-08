6cebb500395819b1b55763f532feee471fcc1966
humwawe <wawe8963098@163.com>
2019-07-08 19:58:38 +0800

    234 235

---
+++ src/main/java/lowest/common/ancestor/of/a/binary/search/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,20 @@
+package lowest.common.ancestor.of.a.binary.search.tree;
+
+import common.TreeNode;
+
+/**
+ * @author hum
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+
+    }
+}
---
+++ src/main/java/palindrome/linked/list/PalindromeLinkedList.java
@@ -0,0 +1,43 @@
+package palindrome.linked.list;
+
+import common.ListNode;
+
+/**
+ * @author hum
+ */
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        ListNode right = reverseList(mid);
+        ListNode left = head;
+        while (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode current = head, next, prev = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
