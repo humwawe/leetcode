eaf08621915bb30f019de14530eaa73c23f2bbcb
humwawe <wawe8963098@163.com>
2019-08-19 19:32:19 +0800

    335

---
+++ src/main/java/self/crossing/SelfCrossing.java
@@ -0,0 +1,21 @@
+package self.crossing;
+
+/**
+ * @author hum
+ */
+public class SelfCrossing {
+    public boolean isSelfCrossing(int[] x) {
+        for (int i = 3; i < x.length; ++i) {
+            if (x[i] >= x[i - 2] && x[i - 3] >= x[i - 1]) {
+                return true;
+            }
+            if (i >= 4 && x[i - 1] == x[i - 3] && x[i] >= x[i - 2] - x[i - 4]) {
+                return true;
+            }
+            if (i >= 5 && x[i - 2] >= x[i - 4] && x[i - 3] >= x[i - 1] && x[i - 1] >= x[i - 3] - x[i - 5] && x[i] >= x[i - 2] - x[i - 4]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
