259a5543d8f7fd45427a94e6e1af1ae44ad414a5
humwawe <wawe8963098@163.com>
2019-09-16 20:15:10 +0800

    391

---
+++ src/main/java/perfect/rectangle/PerfectRectangle.java
@@ -0,0 +1,56 @@
+package perfect.rectangle;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author hum
+ */
+public class PerfectRectangle {
+    public boolean isRectangleCover(int[][] rectangles) {
+        int p1 = Integer.MAX_VALUE;
+        int p2 = Integer.MIN_VALUE;
+        int p3 = Integer.MIN_VALUE;
+        int p4 = Integer.MAX_VALUE;
+
+        Set<String> set = new HashSet<>();
+        int sumArea = 0;
+
+        for (int[] rectangle : rectangles) {
+            p1 = Math.min(p1, rectangle[0]);
+            p4 = Math.min(p4, rectangle[1]);
+            p2 = Math.max(p2, rectangle[2]);
+            p3 = Math.max(p3, rectangle[3]);
+            sumArea += (rectangle[3] - rectangle[1]) * (rectangle[2] - rectangle[0]);
+            String lt = rectangle[0] + " " + rectangle[3];
+            String lb = rectangle[0] + " " + rectangle[1];
+            String rt = rectangle[2] + " " + rectangle[3];
+            String rb = rectangle[2] + " " + rectangle[1];
+            if (!set.contains(lt)) {
+                set.add(lt);
+            } else {
+                set.remove(lt);
+            }
+            if (!set.contains(lb)) {
+                set.add(lb);
+            } else {
+                set.remove(lb);
+            }
+            if (!set.contains(rt)) {
+                set.add(rt);
+            } else {
+                set.remove(rt);
+            }
+            if (!set.contains(rb)) {
+                set.add(rb);
+            } else {
+                set.remove(rb);
+            }
+        }
+
+        if (set.size() == 4 && set.contains(p1 + " " + p3) && set.contains(p1 + " " + p4) && set.contains(p2 + " " + p4) && set.contains(p2 + " " + p3)) {
+            return sumArea == (p2 - p1) * (p3 - p4);
+        }
+        return false;
+    }
+}
