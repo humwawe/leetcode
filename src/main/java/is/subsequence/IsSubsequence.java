2564eb3e001bb284dc02cc7927ddeff1d966de75
humwawe <wawe8963098@163.com>
2019-09-17 18:37:44 +0800

    392

---
+++ src/main/java/is/subsequence/IsSubsequence.java
@@ -0,0 +1,20 @@
+package is.subsequence;
+
+/**
+ * @author hum
+ */
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == s.length();
+    }
+}
