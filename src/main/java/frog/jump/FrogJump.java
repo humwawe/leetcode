6049549e62ba36d86c7c2812e14318072f40a98a
humwawe <wawe8963098@163.com>
2019-09-29 20:27:24 +0800

    403

---
+++ src/main/java/frog/jump/FrogJump.java
@@ -0,0 +1,32 @@
+package frog.jump;
+
+import java.util.*;
+
+/**
+ * @author hum
+ */
+public class FrogJump {
+    public boolean canCross(int[] stones) {
+        int target = stones[stones.length - 1];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int stone : stones) {
+            map.put(stone, new HashSet<>());
+        }
+        map.get(0).add(0);
+        for (int stone : stones) {
+            for (int k : map.get(stone)) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    int num = stone + step;
+                    if (num == target) {
+                        return true;
+                    }
+                    if (step > 0 && map.containsKey(num)) {
+                        map.get(num).add(step);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
