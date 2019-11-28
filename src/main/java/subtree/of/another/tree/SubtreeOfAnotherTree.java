1f11cf6f5d53d7c93f28885629293c46f40f1148
humwawe <wawe8963098@163.com>
2019-11-28 19:43:02 +0800

    572

--- src/main/java/subtree/of/another/tree/SubtreeOfAnotherTree.java
+++ src/main/java/subtree/of/another/tree/SubtreeOfAnotherTree.java
@@ -5,14 +5,25 @@ import common.TreeNode;
 public class SubtreeOfAnotherTree {
     public boolean isSubtree(TreeNode s, TreeNode t) {
         if (s == null) {
+            return false;
+        }
+        return helper(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean helper(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
             return true;
+        }
+        if (s == null || t == null) {
+            return false;
         }
         if (s.val == t.val) {
             boolean l = isSubtree(s.left, t.left);
             boolean r = isSubtree(s.right, t.right);
             return l && r;
         }
-        return isSubtree(s, t.left) || isSubtree(s, t.right);
-
+        return false;
     }
+
+
 }
