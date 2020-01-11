f9d205e9d094878dd814673ffdc48cb1429f40f4
humwawe <wawe8963098@163.com>
2020-01-12 00:18:39 +0800

    745

--- src/main/java/prefix/and/suffix/search/PrefixAndSuffixSearch.java
+++ src/main/java/prefix/and/suffix/search/PrefixAndSuffixSearch.java
@@ -11,16 +11,20 @@ public class PrefixAndSuffixSearch {
     Map<String, Integer> map = new HashMap<>();
 
     public PrefixAndSuffixSearch(String[] words) {
-        for (int i = 1; i < words.length; i++) {
-            String pref = words[i].substring(i);
-            for (int j = words.length - 1; j >= 0; j--) {
-                String suff = words[i].substring(j, words[i].length());
-                map.put(pref + suff, i);
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j <= word.length(); j++) {
+                String pref = word.substring(0, j);
+                for (int k = word.length(); k >= 0; k--) {
+                    String suff = word.substring(k, word.length());
+                    map.put(pref + "#" + suff, i);
+                }
             }
         }
+        System.out.println(map);
     }
 
     public int f(String prefix, String suffix) {
-        return map.getOrDefault(prefix + suffix, 0);
+        return map.getOrDefault(prefix + "#" + suffix, -1);
     }
 }
