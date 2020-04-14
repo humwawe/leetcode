1b3099a91dee7dc14bbabd4612dd66e1bbd3da76
humwawe <wawe8963098@163.com>
2020-04-15 00:24:46 +0800

    972

--- src/main/java/equal/rational/numbers/EqualRationalNumbers.java
+++ src/main/java/equal/rational/numbers/EqualRationalNumbers.java
@@ -8,33 +8,33 @@ public class EqualRationalNumbers {
         if (s.equals(t)) {
             return true;
         }
-        long[] n1 = helper(s);
-        long[] n2 = helper(t);
+        long[] n1 = analysisFraction(s);
+        long[] n2 = analysisFraction(t);
         return n1[0] * n2[1] == n1[1] * n2[0];
     }
 
-    private static long[] helper(String str) {
+
+    private static long[] analysisFraction(String str) {
         int i = str.indexOf(".");
         if (i < 0) {
             return new long[]{Long.parseLong(str), 1};
         }
-        long n = Long.parseLong(str.substring(0, i));
+        long wholeNumber = Long.parseLong(str.substring(0, i));
         str = str.substring(i + 1);
-        i = str.indexOf("(");
-        if (i < 0) {
-            long a = str.length() == 0 ? 0L : Long.parseLong(str);
-            long b = (long) Math.pow(10, str.length());
-            return new long[]{n * b + a, b};
+        int bracketIndex = str.indexOf("(");
+        if (bracketIndex < 0) {
+            long decimal = str.length() == 0 ? 0L : Long.parseLong(str);
+            long denominator = (long) Math.pow(10, str.length());
+            return new long[]{wholeNumber * denominator + decimal, denominator};
         }
-        str = str.substring(0, i);
-        long noCycleOffset = (long) Math.pow(10, str.length());
-        long noCycleNum = (str.length() == 0) ? 0L : Long.parseLong(str);
-        String cycleStr = str.substring(i + 1, str.indexOf(")"));
+        String noCycleStr = str.substring(0, bracketIndex);
+        long noCycleOffset = (long) Math.pow(10, noCycleStr.length());
+        long noCycleNum = (noCycleStr.length() == 0) ? 0L : Long.parseLong(noCycleStr);
+        String cycleStr = str.substring(bracketIndex + 1, str.indexOf(")"));
         long cycleOffset = (long) Math.pow(10, cycleStr.length()) - 1;
         long cycleNum = (cycleStr.length() == 0) ? 0L : Long.parseLong(cycleStr);
-
-        long b = cycleOffset * noCycleOffset;
-        long a = n * b + noCycleNum * cycleOffset + cycleNum;
-        return new long[]{a, b};
+        long denominator = cycleOffset * noCycleOffset;
+        long numerator = wholeNumber * denominator + noCycleNum * cycleOffset + cycleNum;
+        return new long[]{numerator, denominator};
     }
 }
