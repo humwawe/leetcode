2976f991057a7abc0ada851a958d97c5f8769c0b
humwawe <wawe8963098@163.com>
2020-04-25 10:33:51 +0800

    1004

--- src/main/java/max/consecutive/ones/III/MaxConsecutiveOnesIII.java
+++ src/main/java/max/consecutive/ones/III/MaxConsecutiveOnesIII.java
@@ -5,6 +5,22 @@ package max.consecutive.ones.III;
  */
 public class MaxConsecutiveOnesIII {
     public int longestOnes(int[] a, int k) {
-
+        int len = a.length;
+        int j = 0;
+        int count = 0;
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            if (a[i] == 0) {
+                count++;
+            }
+            while (count > k) {
+                if (a[j] == 0) {
+                    count--;
+                }
+                j++;
+            }
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
     }
 }
