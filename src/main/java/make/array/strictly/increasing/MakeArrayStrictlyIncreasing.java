97484ac53a8f87762277c5e4be76aba0861e99b0
humwawe <wawe8963098@163.com>
2020-06-03 21:55:31 +0800

    1187

--- src/main/java/make/array/strictly/increasing/MakeArrayStrictlyIncreasing.java
+++ src/main/java/make/array/strictly/increasing/MakeArrayStrictlyIncreasing.java
@@ -1,13 +1,50 @@
 package make.array.strictly.increasing;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeSet;
 
 /**
  * @author hum
  */
 public class MakeArrayStrictlyIncreasing {
+    Map<String, Integer> map = new HashMap<>();
+    TreeSet<Integer> set = new TreeSet<>();
+
     public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+        for (int i : arr2) {
+            set.add(i);
+        }
+        int len = arr1.length;
+        int result = helper(arr1, 0, -1);
+        if (result == 0x3f3f3f3f) {
+            return -1;
+        }
+        return result;
+    }
+
+    private int helper(int[] arr1, int i, int prev) {
+        if (i == arr1.length) {
+            return 0;
+        }
+        String k = String.valueOf(i) + "#" + String.valueOf(prev);
+        if (map.containsKey(k)) {
+            return map.get(k);
+        }
+        int res = 0x3f3f3f3f;
+        if (arr1[i] > prev) {
+            res = Math.min(res, helper(arr1, i + 1, arr1[i]));
+        }
+        Integer higher = set.higher(prev);
+        if (higher != null) {
+            res = Math.min(res, helper(arr1, i + 1, higher) + 1);
+        }
+        map.put(k, res);
+        return res;
+    }
+
+    public int makeArrayIncreasing2(int[] arr1, int[] arr2) {
         TreeSet<Integer> set = new TreeSet<>();
         for (int i : arr2) {
             set.add(i);
