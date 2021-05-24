19312e31bab4958d2b6ac81ffb7cdf46a074d6cf
humwawe <wawe8963098@163.com>
2021-05-25 00:11:58 +0800

    1871

--- src/main/java/jump/game/VII/JumpGameVII.java
+++ src/main/java/jump/game/VII/JumpGameVII.java
@@ -1,7 +1,32 @@
 package jump.game.VII;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * @author hum
  */
 public class JumpGameVII {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int len = s.length();
+        if (s.charAt(len - 1) == '1') {
+            return false;
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(0);
+        int max = 0;
+        while (!queue.isEmpty()) {
+            Integer cur = queue.poll();
+            if (cur == len - 1) {
+                return true;
+            }
+            for (int i = Math.max(max, cur + minJump); i <= Math.min(cur + maxJump, len - 1); i++) {
+                if (s.charAt(i) == '0') {
+                    queue.add(i);
+                }
+            }
+            max = Math.max(max, cur + maxJump);
+        }
+        return false;
+    }
 }
