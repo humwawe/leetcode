36ea97d7e1cec0b38bdb46c920ff0d310107a71b
humwawe <wawe8963098@163.com>
2022-03-23 22:34:58 +0800

    2213

--- src/main/java/longest/substring/of/one/repeating/character/LongestSubstringOfOneRepeatingCharacter.java
+++ src/main/java/longest/substring/of/one/repeating/character/LongestSubstringOfOneRepeatingCharacter.java
@@ -1,110 +1,110 @@
 package longest.substring.of.one.repeating.character;
 
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.TreeSet;
+
 /**
  * @author hum
  */
 public class LongestSubstringOfOneRepeatingCharacter {
+  TreeSet<Node> set1, set2;
+
   public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
-    chars = (" " + s).toCharArray();
-    build(1, 1, chars.length - 1);
-    int len = queryIndices.length;
-    int[] res = new int[len];
-    for (int i = 0; i < len; i++) {
-      modify(1, queryIndices[i] + 1, queryCharacters.charAt(i));
-      res[i] = query(1, 1, chars.length - 1);
+    set1 = new TreeSet<>(Comparator.comparingInt(x -> x.l));
+    set2 = new TreeSet<>((x, y) -> {
+      int d = x.len - y.len;
+      if (d == 0) {
+        return x.l - y.l;
+      }
+      return d;
+    });
+    char[] chars = s.toCharArray();
+    int len = chars.length;
+    for (int i = 0; i < len; ) {
+      int j = i;
+      while (j + 1 < len && chars[j + 1] == chars[i]) {
+        j++;
+      }
+      set1.add(new Node(i, j));
+      set2.add(new Node(i, j));
+      i = j + 1;
+    }
+    int[] res = new int[queryCharacters.length()];
+    for (int i = 0; i < queryCharacters.length(); i++) {
+      int idx = queryIndices[i];
+      char c = queryCharacters.charAt(i);
+      if (c == chars[idx]) {
+        res[i] = set2.last().len;
+        continue;
+      }
+      chars[idx] = c;
+      del(idx);
+      del(idx + 1);
+      if (idx > 0 && chars[idx - 1] == c) {
+        merge(idx);
+      }
+      if (idx + 1 < len && chars[idx + 1] == c) {
+        merge(idx + 1);
+      }
+      res[i] = set2.last().len;
     }
     return res;
   }
 
-  int N = (int) (1e5 + 5);
-  Node[] tr = new Node[N * 4];
-  char[] chars;
-
-  void pushUp(int u) {
-    Node l = tr[u << 1];
-    Node r = tr[u << 1 | 1];
-    Node cur = tr[u];
-    cur.max = Math.max(l.max, r.max);
-    cur.lmax = l.lmax;
-    cur.rmax = r.rmax;
-    cur.lc = l.lc;
-    cur.rc = r.rc;
-
-    if (l.rc == r.lc) {
-      cur.max = Math.max(cur.max, l.rmax + r.lmax);
-      if (l.lmax == l.r - l.l + 1) {
-        cur.lmax += r.lmax;
-      }
-      if (r.rmax == r.r - r.l + 1) {
-        cur.rmax += l.rmax;
-      }
-    }
+  private void merge(int idx) {
+    Node tmp = new Node(idx, idx);
+    Node floor = set1.floor(tmp);
+    Node lower = set1.lower(tmp);
+    set1.remove(floor);
+    set2.remove(floor);
+    set1.remove(lower);
+    set2.remove(lower);
+    set1.add(new Node(lower.l, floor.r));
+    set2.add(new Node(lower.l, floor.r));
   }
 
-  void build(int u, int l, int r) {
-    if (l == r) {
-      tr[u] = new Node(l, r, 1, chars[l], 1, chars[r], 1);
+  private void del(int idx) {
+    Node floor = set1.floor(new Node(idx, idx));
+    if (floor.l == idx || idx > floor.r) {
       return;
     }
-    tr[u] = new Node(l, r);
-    int mid = l + r >> 1;
-    build(u << 1, l, mid);
-    build(u << 1 | 1, mid + 1, r);
-    pushUp(u);
-  }
-
-  int query(int u, int l, int r) {
-    if (tr[u].l >= l && tr[u].r <= r) {
-      return tr[u].max;
-    }
-    int mid = tr[u].l + tr[u].r >> 1;
-    int v = Integer.MIN_VALUE;
-    if (l <= mid) {
-      v = query(u << 1, l, r);
-    }
-    if (r > mid) {
-      v = Math.max(v, query(u << 1 | 1, l, r));
-    }
-    return v;
-  }
-
-  void modify(int u, int x, char c) {
-    if (tr[u].l == x && tr[u].r == x) {
-      tr[u].lc = c;
-      tr[u].rc = c;
-      return;
-    }
-    int mid = tr[u].l + tr[u].r >> 1;
-    if (x <= mid) {
-      modify(u << 1, x, c);
-    } else {
-      modify(u << 1 | 1, x, c);
-    }
-    pushUp(u);
+    set1.remove(floor);
+    set2.remove(floor);
+    set1.add(new Node(floor.l, idx - 1));
+    set2.add(new Node(floor.l, idx - 1));
+    set1.add(new Node(idx, floor.r));
+    set2.add(new Node(idx, floor.r));
   }
 
   class Node {
     int l;
     int r;
-    int lmax;
-    char lc;
-    int rmax;
-    char rc;
-    int max;
+    int len;
 
     public Node(int l, int r) {
       this.l = l;
       this.r = r;
+      this.len = r - l + 1;
     }
 
-    public Node(int l, int r, int lmax, char lc, int rmax, char rc, int max) {
-      this.l = l;
-      this.r = r;
-      this.lmax = lmax;
-      this.lc = lc;
-      this.rmax = rmax;
-      this.rc = rc;
-      this.max = max;
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof Node)) {
+        return false;
+      }
+      Node node = (Node) o;
+      return l == node.l && r == node.r && len == node.len;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(l, r, len);
     }
   }
+
 }
+
